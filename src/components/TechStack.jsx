d6e7a000c63681e00e6f9e115237c81b3951688c
poseidonsanket <sanketdadali05@gmail.com>
2024-02-20 21:59:40 +0530

    Accordian Updated

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -5,41 +5,47 @@ const TechStack = () => {
   const [frontEnd, setFrontEnd] = useState(false);
   const [backEnd, setBackEnd] = useState(false);
   const [database, setDatabase] = useState(false);
-  const [devops, setDevops] = useState(false);
+  const [core, setCore] = useState(false);
   return (
     <div className="tech-main">
       <h1 className="tech-heading">Tech Stack</h1>
       <div className="tech-accordian-main">
         <div>
-          <div className="tech-accordian" onClick={()=> setFrontEnd(!frontEnd)}>
+          <div className="tech-accordian" onClick={() => setCore(!core)}>
+            <span>Core Languages</span>
+            <span style={{ fontSize: "1.7rem" }}>{core ? "-" : "+"}</span>
+          </div>
+          {core && <p>Still Learning 🥲</p>}
+        </div>
+
+        <div>
+          <div
+            className="tech-accordian"
+            onClick={() => setFrontEnd(!frontEnd)}
+          >
             <span>Front-End</span>
-            <span style={{fontSize: "1.7rem"}}>{frontEnd?"-":"+"}</span>
+            <span style={{ fontSize: "1.7rem" }}>{frontEnd ? "-" : "+"}</span>
           </div>
           {frontEnd && <p>Still Learning 🥲</p>}
         </div>
 
         <div>
-          <div className="tech-accordian" onClick={()=> setBackEnd(!backEnd)}>
-            <span>Back-End</span>
-            <span style={{fontSize: "1.7rem"}}>{backEnd?"-":"+"}</span>
+          <div className="tech-accordian" onClick={() => setBackEnd(!backEnd)}>
+            <span>Back-End & DevOps</span>
+            <span style={{ fontSize: "1.7rem" }}>{backEnd ? "-" : "+"}</span>
           </div>
           {backEnd && <p>Still Learning 🥲</p>}
         </div>
 
         <div>
-          <div className="tech-accordian" onClick={()=> setDatabase(!database)}>
-            <span>Databases</span>
-            <span style={{fontSize: "1.7rem"}}>{database?"-":"+"}</span>
+          <div
+            className="tech-accordian"
+            onClick={() => setDatabase(!database)}
+          >
+            <span>DataBases</span>
+            <span style={{ fontSize: "1.7rem" }}>{database ? "-" : "+"}</span>
           </div>
           {database && <p>Still Learning 🥲</p>}
-        </div>
-
-        <div>
-          <div className="tech-accordian" onClick={()=> setDevops(!devops)}>
-            <span>DevOps</span>
-            <span style={{fontSize: "1.7rem"}}>{devops?"-":"+"}</span>
-          </div>
-          {devops && <p style={{display: "flex", justifyContent: "center" , fontSize:"1.5rem"}}>Still Learning 🥲</p>}
         </div>
       </div>
     </div>
