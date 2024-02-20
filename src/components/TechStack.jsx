ce6227a8959e4800d800c8b6ff1bad2f383b45de
poseidonsanket <sanketdadali05@gmail.com>
2024-02-20 21:39:07 +0530

    TechStack Styling Done

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import "../css/TechStack.css"
+import React from "react";
+import "../css/TechStack.css";
 
 const TechStack = () => {
-    const techList = ["Front-End","Back-End","Databases","DevOps"]
   return (
     <div className="tech-main">
-        <h1 className="tech-heading">Tech Stack</h1>
-        {techList.map((tech) => <div className="tech-accordian">tech</div>)}
-    </div>
-  )
-}
+      <h1 className="tech-heading">Tech Stack</h1>
+      <div className="tech-accordian-main">
+        <div className="tech-accordian">
+          <span>Front-End</span>
+          <span>+</span>
+        </div>
 
-export default TechStack
+        <div className="tech-accordian">
+          <span>Back-End</span>
+          <span>+</span>
+        </div>
+
+        <div className="tech-accordian">
+          <span>DataBases</span>
+          <span>+</span>
+        </div>
+
+        <div className="tech-accordian">
+          <span>DevOps</span>
+          <span>+</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TechStack;
