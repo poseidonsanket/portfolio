abe7a5218e257b1dc33cb721e8161cd1118df17b
poseidonsanket <sanketdadali05@gmail.com>
2024-02-20 21:53:50 +0530

    Accordian Created

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,29 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/TechStack.css";
 
 const TechStack = () => {
+  const [frontEnd, setFrontEnd] = useState(false);
+  const [backEnd, setBackEnd] = useState(false);
+  const [database, setDatabase] = useState(false);
+  const [devops, setDevops] = useState(false);
   return (
     <div className="tech-main">
       <h1 className="tech-heading">Tech Stack</h1>
       <div className="tech-accordian-main">
-        <div className="tech-accordian">
-          <span>Front-End</span>
-          <span>+</span>
+        <div>
+          <div className="tech-accordian" onClick={()=> setFrontEnd(!frontEnd)}>
+            <span>Front-End</span>
+            <span style={{fontSize: "1.7rem"}}>{frontEnd?"-":"+"}</span>
+          </div>
+          {frontEnd && <p>Still Learning 🥲</p>}
         </div>
 
-        <div className="tech-accordian">
-          <span>Back-End</span>
-          <span>+</span>
+        <div>
+          <div className="tech-accordian" onClick={()=> setBackEnd(!backEnd)}>
+            <span>Back-End</span>
+            <span style={{fontSize: "1.7rem"}}>{backEnd?"-":"+"}</span>
+          </div>
+          {backEnd && <p>Still Learning 🥲</p>}
         </div>
 
-        <div className="tech-accordian">
-          <span>DataBases</span>
-          <span>+</span>
+        <div>
+          <div className="tech-accordian" onClick={()=> setDatabase(!database)}>
+            <span>Databases</span>
+            <span style={{fontSize: "1.7rem"}}>{database?"-":"+"}</span>
+          </div>
+          {database && <p>Still Learning 🥲</p>}
         </div>
 
-        <div className="tech-accordian">
-          <span>DevOps</span>
-          <span>+</span>
+        <div>
+          <div className="tech-accordian" onClick={()=> setDevops(!devops)}>
+            <span>DevOps</span>
+            <span style={{fontSize: "1.7rem"}}>{devops?"-":"+"}</span>
+          </div>
+          {devops && <p style={{display: "flex", justifyContent: "center" , fontSize:"1.5rem"}}>Still Learning 🥲</p>}
         </div>
       </div>
     </div>
