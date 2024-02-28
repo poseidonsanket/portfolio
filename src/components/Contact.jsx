52656f0293dc967ad93d421abeee1983151757b8
poseidonsanket <sanketdadali05@gmail.com>
2024-02-28 16:36:33 +0530

    Contact Form Added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,9 +8,7 @@ const Contact = () => {
         <h1>Contact</h1>
       </div>
       <div className="contact-s-1">
-        <p className="contact-heading-2">
-          Don't be shy! Hit me up! 👇
-        </p>
+        <p className="contact-heading-2">Don't be shy! Hit me up! 👇</p>
       </div>
       <div className="contact-icons">
         <div className="contact-icons-1">
@@ -62,6 +60,48 @@ const Contact = () => {
             <a href="mailto:sanketdadali05@gamil.com">
               sanketdadali05@gamil.com
             </a>
+          </div>
+        </div>
+      </div>
+      <div className="contact-container">
+        <div class="container">
+          <div class="screen">
+            <div class="screen-header">
+              <div class="screen-header-left">
+                <div class="screen-header-button close"></div>
+                <div class="screen-header-button maximize"></div>
+                <div class="screen-header-button minimize"></div>
+              </div>
+              <div class="screen-header-right">
+                <div class="screen-header-ellipsis"></div>
+                <div class="screen-header-ellipsis"></div>
+                <div class="screen-header-ellipsis"></div>
+              </div>
+            </div>
+            <div class="screen-body">
+              <div class="screen-body-item left">
+                <div class="app-title">
+                  <span>CONTACT</span>
+                </div>
+              </div>
+              <div class="screen-body-item">
+                <div class="app-form">
+                  <div class="app-form-group">
+                    <input class="app-form-control" placeholder="NAME" />
+                  </div>
+                  <div class="app-form-group">
+                    <input class="app-form-control" placeholder="EMAIL" />
+                  </div>
+                  <div class="app-form-group message">
+                    <input class="app-form-control" placeholder="MESSAGE" />
+                  </div>
+                  <div class="app-form-group buttons">
+                    <button class="app-form-button">CANCEL</button>
+                    <button class="app-form-button">SEND</button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
