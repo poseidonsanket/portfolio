ea65fbb87d295fbe7ee9a8c5152f22c0da8c1920
poseidonsanket <sanketdadali05@gmail.com>
2024-03-28 18:46:25 +0530

    New NextJs Project Added

--- src/utils/ProjectConstants.jsx
+++ src/utils/ProjectConstants.jsx
@@ -1,5 +1,6 @@
 import NetflixGpt from "../img/NetflixGpt.png";
 import FitLifeHub from "../img/FitLifeHub.png";
+import MeetingsApp from "../img/MeetingsApp.png";
 
 const reactSvg = (
   <img
@@ -70,6 +71,66 @@ const materialUi = (
   </svg>
 );
 
+const nextjsSvg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    x="8"
+    y="8"
+    width="16"
+    height="16"
+    viewBox="0 0 48 48"
+  >
+    <path
+      fill="#fff"
+      d="M18.974,31.5c0,0.828-0.671,1.5-1.5,1.5s-1.5-0.672-1.5-1.5v-14c0-0.653,0.423-1.231,1.045-1.43 c0.625-0.198,1.302,0.03,1.679,0.563l16.777,23.704C40.617,36.709,44,30.735,44,24c0-11-9-20-20-20S4,13,4,24s9,20,20,20 c3.192,0,6.206-0.777,8.89-2.122L18.974,22.216V31.5z M28.974,16.5c0-0.828,0.671-1.5,1.5-1.5s1.5,0.672,1.5,1.5v13.84l-3-4.227 V16.5z"
+    ></path>
+  </svg>
+);
+
+const htmlSVG = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    x="9"
+    y="7"
+    width="16"
+    height="16"
+    viewBox="0 0 48 48"
+  >
+    <path fill="#E65100" d="M41,5H7l3,34l14,4l14-4L41,5L41,5z"></path>
+    <path fill="#FF6D00" d="M24 8L24 39.9 35.2 36.7 37.7 8z"></path>
+    <path
+      fill="#FFF"
+      d="M24,25v-4h8.6l-0.7,11.5L24,35.1v-4.2l4.1-1.4l0.3-4.5H24z M32.9,17l0.3-4H24v4H32.9z"
+    ></path>
+    <path
+      fill="#EEE"
+      d="M24,30.9v4.2l-7.9-2.6L15.7,27h4l0.2,2.5L24,30.9z M19.1,17H24v-4h-9.1l0.7,12H24v-4h-4.6L19.1,17z"
+    ></path>
+  </svg>
+);
+
+const cssSvg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    x="9"
+    y="7"
+    width="16"
+    height="16"
+    viewBox="0 0 48 48"
+  >
+    <path fill="#0277BD" d="M41,5H7l3,34l14,4l14-4L41,5L41,5z"></path>
+    <path fill="#039BE5" d="M24 8L24 39.9 35.2 36.7 37.7 8z"></path>
+    <path
+      fill="#FFF"
+      d="M33.1 13L24 13 24 17 28.9 17 28.6 21 24 21 24 25 28.4 25 28.1 29.5 24 30.9 24 35.1 31.9 32.5 32.6 21 32.6 21z"
+    ></path>
+    <path
+      fill="#EEE"
+      d="M24,13v4h-8.9l-0.3-4H24z M19.4,21l0.2,4H24v-4H19.4z M19.8,27h-4l0.3,5.5l7.9,2.6v-4.2l-4.1-1.4L19.8,27z"
+    ></path>
+  </svg>
+);
+
 const allProjects = [
   {
     title: "NetflixGpt",
@@ -103,6 +164,22 @@ const allProjects = [
     type: "react",
     gitLink: "https://github.com/poseidonsanket/FitnessApp.git",
     liveLink: "https://fitlifehub12.netlify.app/",
+  },
+  {
+    title: "Meetings App",
+    desc: "The Next.js Meetings App provides users with the ability to effortlessly add new meetings, view existing meetings, and access detailed descriptions of each meeting with just a click. Through dynamic routing, users can navigate between different meeting pages, ensuring a smooth and intuitive browsing experience.",
+    img: MeetingsApp,
+    techStack: [
+      { logo: nextjsSvg, name: "NEXTJS" },
+      { logo: htmlSVG, name: "HTML" },
+      {
+        logo: cssSvg,
+        name: "CSS",
+      },
+    ],
+    type: "next",
+    gitLink: "https://github.com/poseidonsanket/meetings-app.git",
+    liveLink: "https://meetings-app-tau.vercel.app/",
   },
 ];
 
